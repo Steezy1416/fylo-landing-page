f77018be834c2ae2814d9e70cb88fb71693144b4
alexfortin <fortin.alex14@gmail.com>
2023-12-16 12:17:11 -0600

    styled the icon section and made it responsive, also change the main bg color varable to match the color of the decorative element

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Banner from "./components/banner/Banner";
 import Footer from "./components/Footer";
 import GetAccessSection from "./components/GetAccessSection";
 import Header from "./components/header/Header";
-import IconSection from "./components/IconSection";
+import IconSection from "./components/iconSection/IconSection";
 import ProductiveSection from "./components/ProductiveSection";
 import ReviewSection from "./components/ReviewSection";
 import "./style.css"
--- src/images/index.js
+++ src/images/index.js
@@ -4,7 +4,11 @@ import productiveImage from "./illustration-stay-productive.png";
 import profileOne from "./profile-1.jpg";
 import profileTwo from "./profile-2.jpg";
 import profileThree from "./profile-3.jpg";
-import logo from "./logo.svg"
+import logo from "./logo.svg";
+import accessIcon from "./icon-access-anywhere.svg";
+import securityIcon from "./icon-security.svg";
+import realTimeIcon from "./icon-collaboration.svg";
+import anyFileIcon from "./icon-any-file.svg";
 
 export {
   bgQuotes,
@@ -13,5 +17,9 @@ export {
   profileOne,
   profileTwo,
   profileThree,
-  logo
+  logo,
+  accessIcon,
+  securityIcon,
+  realTimeIcon,
+  anyFileIcon,
 };
